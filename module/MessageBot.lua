a5456e549e8845d5331e85e06770cdf69894f78f
Muzixi <voidwind@qq.com>
2025-10-29 01:45:22 +0800

    Add Message Bot Module Say "123" InviteUnit

--- module/MessageBot.lua
+++ module/MessageBot.lua
@@ -1,3 +1,6 @@
 function VoidFrame:MessageStart(text, playerName, languageName, channelName)
     print(channelName .. "|cFA500FF0" .. playerName .. "|r say: " .. text)
+    if text == "123" then
+        C_PartyInfo.InviteUnit(playerName)
+    end
 end
