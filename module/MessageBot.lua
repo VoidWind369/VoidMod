d32c866a3c8a3869ab44728699386ad2fbc2c5c6
Muzixi <voidwind@qq.com>
2026-03-28 01:07:32 +0800

    优化

--- module/MessageBot.lua
+++ module/MessageBot.lua
@@ -1,17 +1,24 @@
 function VoidFrame:MessageStart(text, playerName, languageName, channelName)
+    if not text then
+        return
+    end
     print(channelName .. "|cFA500FF0" .. playerName .. "|r say: " .. text)
     if text == "123" then
         C_PartyInfo.InviteUnit(playerName)
-    elseif text == "团队" then
+    end
+    if text == "团队" then
         C_PartyInfo.ConfirmConvertToRaid()
-    elseif text == "小队" then
+    end
+    if text == "小队" then
         C_PartyInfo.ConvertToParty()
-    elseif string.find(text, "倒数") then
+    end
+    if string.find(text, "倒数") then
         local num_str = string.gsub(text, "倒数", "")
         local num = tonumber(strtrim(num_str)) or 10
         print("|cFFFF0000开始倒计时|r", num, "|cFFFF0000秒|r")
         C_PartyInfo.DoCountdown(num)
-    elseif text == "属性" then
+    end
+    if text == "属性" then
         SendChatMessage(self:Void_PlayerInfo(), "WHISPER", nil, playerName)
     end
 end
